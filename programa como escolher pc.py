36abb35c5c2757102d2778b8b0f0bb75aab6727e
nextstage <isaacppedrosocavalcante@gmail.com>
2025-08-24 19:27:41 -0300

    erro absurdo

---
+++ programa como escolher pc.py
@@ -0,0 +1,26 @@
+#23/08/2025
+#programador isaac pedroso
+print("como escolher peças de um pc")
+orcamento=0
+
+
+
+if orcamento <=3900:
+    print("Vaí trabalhar.")
+    elif orcamento > 4000:
+    inicio=input(deseja começar?(S or N) )
+    elif inicio==N:
+    print("vaza então Zé")        
+    elif inicio==S:                
+    processador=input("vamos começar pelo processador(confirma?) (S or N)")                
+    elif processador==S:                    
+    print("bora lá, com video integrado atualmente os intel 12400f ou 14400f ou 14600kf se tormanam otimas oções")                       
+    elif processador==N:                         
+    print("pois então vaza pq vai ser na ordem que eu quiser.")                           
+    elif processador==S:                                
+    placamae=input("agora bora pra mobo ? mais conhecida por placa mãe(S or N)")                                
+                                            
+                                            
+                                                
+                                                    
+                                                    
